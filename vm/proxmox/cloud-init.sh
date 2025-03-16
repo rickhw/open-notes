aca3db55cc709b40e809f4b70994a6e75c949381
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-16 09:55:04 +0800

    update on 20250316-0955, comment:

--- vm/proxmox/cloud-init.sh
+++ vm/proxmox/cloud-init.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 # ubuntu 20.04, java application server
 
-sudo apt install git curl zip unzip htop btop -y
+sudo apt install vim git curl zip unzip htop btop -y
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java
+sdk install java 17.0.9-graalce
+sdk install java 17.0.14-amzn
+sdk install java 17.0.14-zulu 
 sdk install gradle
 sdk install maven
 
