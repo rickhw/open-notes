fc7281c0f48491ebc834fa912c0db559c5b13a40
Rick Hwang <rick_kyhwang@hotmail.com>
2025-07-15 19:42:25 +0800

    update on 20250715-1942, comment:

--- vm/proxmox/cloud-init.sh
+++ vm/proxmox/cloud-init.sh
@@ -7,8 +7,8 @@ curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk install java 17.0.9-graalce
-sdk install java 17.0.14-amzn
-sdk install java 17.0.14-zulu 
+#sdk install java 17.0.14-amzn
+#sdk install java 17.0.14-zulu 
 sdk install gradle
 sdk install maven
 
